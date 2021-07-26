0052079047ae1114781be3bb6733387c60df9020
Alanasdw <alanasdw@gmail.com>
2021-07-26 22:44:17 +0800

    Fix: removed some unused comments

--- read_sav.py
+++ read_sav.py
@@ -3,15 +3,6 @@ import pandas
 
 df, meta = pyreadstat.read_sav('../KIT3月齡組第1波3月齡家長_final.sav')
 
-# done! let's see what we got
-# print(df.head())
-# print(meta.column_names)
-# print(meta.column_labels)
-# print(meta.column_names_to_labels)
-# print(meta.number_rows)
-# print(meta.number_columns)
-# print(meta.file_label)
-# print(meta.file_encoding)
 # print(meta.original_variable_types) # this is for the data type reffered by sav files
 # there are other metadata pieces extracted. See the documentation for more details.
 
