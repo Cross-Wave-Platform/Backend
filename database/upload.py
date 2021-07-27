34b1af30e25f8d249835f393ba28592aa0b42196
Willy-chen <willychenwii@gmail.com>
2021-07-27 03:03:11 +0000

    feat: remove requset from flask

--- database/upload.py
+++ database/upload.py
@@ -1,4 +1,4 @@
-import flask
+import os
 import pandas as pd
 import pyreadstat as prs
 from werkzeug.utils import secure_filename
@@ -11,21 +11,21 @@ def allowed_file(filename):
 
 class Upload_Files():
     def __init__(self, username):
-        self.user = username
+        self.username = username
 
     def get_user_folder(self):
         #get user folder path
-        file_dir = os.path.join(fileApp_api.config['UPLOAD_FOLDER'] , self.user)
+        file_dir = os.path.join(fileApp_api.config['UPLOAD_FOLDER'] , self.username)
         #create user folder if not exist
         if not os.path.exists(file_dir):
                 os.makedirs(file_dir)
         return file_dir
 
-    def get_file(self, request):
+    def get_user_file(self, request_files):
         # check if the post request has the file part
-        if 'file' not in request.files:
+        if 'file' not in request_files:
             return "No files"
-        file = request.files['file']
+        file = request_files['file']
 
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
