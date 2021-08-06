dad76620b1f81b8cab828fe99f55c5a7ee16ddd8
Willy-chen <willychenwii@gmail.com>
2021-08-06 13:34:29 +0000

    add: SearchApp and search feat: abstract of SearchWave, SearchInfo, StoreInfo, GetInfo

---
+++ controller/SearchApp.py
@@ -0,0 +1,58 @@
+from flask import Blueprint
+from flask_login import login_required
+from service.account import Account
+from service.search import search_wave, search_info, store_info, get_info
+from .utils.response import HTTPResponse, HTTPError
+from .utils.request import Request
+
+__all__ = ['SearchApp_api']
+
+SearchApp_api = Blueprint('SearchApp_api',__name__)
+
+
+@SearchApp_api.route('/SearchWave', methods=['GET'])
+@Request.json('age_type: int','survey_type: int')
+def searchWave(age_type, survey_type):
+    try:
+        wave = search_wave(age_type, survey_type)
+        if wave == 'not found':
+            return HTTPError('Wave not found', 404)
+    except:
+        return HTTPError('unknown error', 406)
+
+    return HTTPResponse('ok', wave=wave)
+
+@SearchApp_api.route('/SearchInfo', methods=['GET'])
+@Request.json('age_type: int', 'survey_type: int', 'wave: int')
+def searchWave(age_type, survey_type, wave):
+    try:
+        Info = search_info(age_type,survey_type, wave)
+        if Info == 'not found':
+            return HTTPError('Info not found', 404)
+    except:
+        return HTTPError('unknown error', 406)
+    
+    return HTTPResponse('ok', info=Info)
+
+@SearchApp_api.route('/StoreInfo', methods=['POST'])
+@login_required
+@Request.json('problem_id: str[]')
+def storeInfo(problem_id):
+    try:
+        res = store_info(problem_id)
+        # if res == 'failed':
+        #     return HTTPError('Failed to store info', 403)
+    except:
+        return HTTPError('unknown error', 406)
+    return HTTPResponse('ok')
+
+@SearchApp_api.route('/GetInfo', methods=['GET'])
+@login_required
+def getInfo():
+    try:
+        problem_id = get_info()
+        if problem_id == 'failed':
+            return HTTPError('Failed to fetch info', 403)
+    except:
+        return HTTPError('unknown error', 406)
+    return HTTPResponse('ok', problem_id=problem_id)
---
+++ service/search.py
@@ -0,0 +1,31 @@
+
+
+__all__ = ['']
+
+def search_wave(age_type, survey_type):
+    wave = []
+    '''
+    sql search for wave
+    wave = str['wave', ...]
+    '''
+    return wave
+
+def search_info(age_type, survey_type, wave):
+    info = [{'problem_id':'str', 'topic':'str', 'tag':[{'tag_value':'int', 'tag_name':'str'},], 'class':'str', 'exist':[{'type':'int', 'wave':{'young':[{'int':'int'}],'old':[{'int':'int'}]}}]}]
+    '''
+    sql search for info
+    '''
+    return info
+
+def store_info(problem_id):
+    '''
+    sql store to shopping cart
+    '''
+    return "Success"
+
+def get_info():
+    problem_id = []
+    '''
+    sql get shopping cart info
+    '''
+    return problem_id
