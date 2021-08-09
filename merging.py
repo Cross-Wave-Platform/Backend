4b7cb8f76af75a3fdec61fea0d4e092ecf5099ba
Alanasdw <alanasdw@gmail.com>
2021-08-09 22:09:02 +0800

    Feature: added file check and update column usage
    columns usage can now be seperate

--- merging.py
+++ merging.py
@@ -2,17 +2,24 @@ from typing import List
 from pandas.core.reshape.merge import merge
 import pyreadstat
 import pandas
+import os
 
 # for memory usage
 import resource
 
-def merger(file_names: List[str], used_columns: List[str], merge_type: str, file_type: str) -> None:
+def merger(file_names: List[str], used_columns: List[List[str]], merge_type: str, file_type: str) -> bool:
+
+    # check file exists
+    for item in file_names:
+        if os.path.isfile(item) == False:
+            print(item)
+            return False
 
     dataframes = []
     metas = []
 
-    for i in file_names:
-        temp_df, temp_meta = pyreadstat.read_sav(i, usecols=used_columns)# 
+    for i in range(len(file_names)):
+        temp_df, temp_meta = pyreadstat.read_sav(file_names[i], usecols=used_columns[i])# 
         dataframes.append(temp_df)
         metas.append(temp_meta)
 
@@ -31,22 +38,22 @@ def merger(file_names: List[str], used_columns: List[str], merge_type: str, file
 
         # result = pandas.merge( left=dataframes[0], right=dataframes[1], how=merge_type, on=['baby_id'], suffixes=['_M3','_M6'])
 
-    # for i in dataframes:
-    #     print(i)
+    for i in dataframes:
+        print(i)
     print(result)
     # result.to_csv('testing.csv', index=False)
 
-    return
+    return True
 
 
 if __name__ == "__main__":
     # example
-    file_names = ['./KIT3月齡組第1波3月齡家長_final.sav', './KIT3月齡組第2波6月齡家長_final.sav']# , './KIT3月齡組第1波3月齡親友_final.sav'
-    file_names = [ item for item in file_names for repet in range(10)]
-    print(file_names)
-    used_columns = ['baby_id', 'pfa0101', 'pfa0102']
+    file_names = ['../KIT3月齡組第1波3月齡家長_final.sav', '../KIT3月齡組第2波6月齡家長_final.sav']# , './KIT3月齡組第1波3月齡親友_final.sav'
+    # file_names = [ item for item in file_names for repet in range(10)]
+    # print(file_names)
+    used_columns = [['baby_id', 'pfa0101', 'pfa0102'], ['baby_id','pfa0201']]
 
-    merger(file_names,used_columns,'inner','csv')
+    print( merger(file_names,used_columns,'inner','csv'))
 
     print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss, 'KB')
 
