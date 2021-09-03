5b1731eed1ff1e5563bbdb6f05c138653263ca87
Willy-chen <willychenwii@gmail.com>
2021-09-03 09:13:20 +0000

    add: time and date to report email

--- controller/reportApp.py
+++ controller/reportApp.py
@@ -4,6 +4,7 @@ from flask_login import login_required
 from service.report import Send_Email
 from .utils.response import HTTPResponse, HTTPError
 from .utils.request import Request
+from datetime import datetime
 
 __all__ = ['reportApp_api']
 
@@ -16,7 +17,7 @@ admin_email = 'admin@gmail.com' ##TBC
 @Request.files('file')
 @Request.form('title', 'content')
 def upload_file(file, title, content):
-    mymail = Send_Email(title,[admin_email])
+    mymail = Send_Email(title+datetime.now().strftime('%Y/%m/%d %H:%M:%S'),[admin_email])
     try:
         mymail.htmladd(content)
         mymail.addattach(file)   
