ca4e880d380a04c932312878771b1348206a63df
Alanasdw <alanasdw@gmail.com>
2021-09-11 14:11:08 +0800

    End semi_merging with concat/uploadProblem
    currently the upload problem will not work since there is a collision in the file

--- repo/upload/survey.py
+++ repo/upload/survey.py
@@ -5,8 +5,23 @@ import pandas
 from pandas.core.frame import DataFrame
 from ..manager import SQLManager
 
-# what is '3月齡組\n18月齡（已回補）', does '（已回補）' have meaning??
+class SheetCountError(Exception):
+    pass
 
+class InvalidName(Exception):
+    pass
+
+class MissingSheet(Exception):
+    pass
+
+class ColumnNameError(Exception):
+    pass
+
+class DataError(Exception):
+    pass
+
+class ProblemCollision(Exception):
+    pass
 
 class SurveyUpload(SQLManager):
     def upload(self, xlsx_path: str):
@@ -16,33 +31,26 @@ class SurveyUpload(SQLManager):
             with open(xlsx_path, 'rb') as file:
                 all_data = pandas.read_excel(file, sheet_name=None)
         except IOError as exec:
-            return exec.strerror
+            raise exec
 
         # print(all_data)
 
         # suppose there are three sheets [家長問卷,教保問卷,親友問卷]
         # need to change the code here to have more references
         if len(all_data) != 3:
-            return 'sheet_count_error'
+            raise SheetCountError
 
         surveys = []
         # get the parent part
-        # this is not usable since the file format is not settled
         temp = self.get_survey('家長問卷', all_data)
-        if type(temp) == str:
-            return temp
         surveys.append(temp)
 
         # get the friend part
         temp = self.get_survey('親友問卷', all_data)
-        if type(temp) == str:
-            return temp
         surveys.append(temp)
 
         # # get the teacher part
         temp = self.get_survey('教保問卷', all_data)
-        if type(temp) == str:
-            return temp
         surveys.append(temp)
 
         # flatten
@@ -63,7 +71,7 @@ class SurveyUpload(SQLManager):
         surveys = surveys[[
             'survey_id', 'age_type', 'survey_type', 'wave', 'release'
         ]]
-        print(surveys)
+        # print(surveys)
         # insert to database
         self.bulk_insert(surveys, 'dbo.survey')
 
@@ -71,7 +79,6 @@ class SurveyUpload(SQLManager):
         # get the current classes
         command = ("SELECT * FROM class;")
         current_class = pandas.read_sql(command, self.conn)
-        # print(current_class)
 
         classes = pandas.DataFrame(columns=['class', 'subclass'])
 
@@ -90,7 +97,7 @@ class SurveyUpload(SQLManager):
                      inplace=True)
         classes['class_id'] = ''
         classes = classes[['class_id', 'class', 'subclass']]
-        print(classes)
+        # print(classes)
         # insert ot database
         self.bulk_insert(classes, 'dbo.class')
         # get the current classes
@@ -106,8 +113,9 @@ class SurveyUpload(SQLManager):
             columns=['problem_name', 'topic', 'class', 'subclass'])
 
         problems = problems.append(self.get_problem('家長問卷', all_data))
-        # problems = problems.append( self.get_problem('親友問卷', all_data))
-        # problems = problems.append( self.get_problem('教保問卷', all_data))
+        problems = problems.append( self.get_problem('親友問卷', all_data))
+        problems = problems.append( self.get_problem('教保問卷', all_data))
+        problems = problems.reset_index(drop=True)
 
         problems.drop_duplicates(inplace=True)
         problems['problem_name'] = problems['problem_name'].str.lower()
@@ -125,7 +133,7 @@ class SurveyUpload(SQLManager):
         # sanity check
         sanity = problems.groupby('problem_name').size().to_list()
         if sanity and max(sanity) != 1:
-            return 'problem_collision'
+            raise ProblemCollision
 
         # give it problem_id and sort the columns
         problems['problem_id'] = ''
@@ -141,7 +149,6 @@ class SurveyUpload(SQLManager):
 
         return 'success'
 
-    # need to tweek the numbers of columns that need to be chooped off
     def get_survey(self, sheet_name: str, all_data: dict):
         # check the sheet name and set survey_type
         survey_type = 0
@@ -152,12 +159,12 @@ class SurveyUpload(SQLManager):
         elif sheet_name == '親友問卷':
             survey_type = 3
         else:
-            return sheet_name + '_invalid_name'
+            raise InvalidName
 
         # get the page
         page = all_data.get(sheet_name, "not_found")
         if type(page) == str and page == "not_found":
-            return sheet_name + '_missing'
+            raise MissingSheet
 
         # pandas.DataFrame(columns=['age_type','survey_type','wave','release'])
         # convert to dataframe later
@@ -172,30 +179,17 @@ class SurveyUpload(SQLManager):
             elif age_type == '36':
                 age_type = 2
             elif 'Unnamed' in age_type:
-                print(age_type)
-                return 'unexpected_data_error'
+                raise DataError
             else:
-                print(age_type)
-                return '月齡組_error'
+                raise ColumnNameError
 
-            # this is a full sized '('
             # need to restrict this kind of usage
             wave = string.split('\n')[1]
 
             survey.append([age_type, survey_type, wave])
 
-        # # the release part
-        # release_row = [ i for sublist in page.iloc[-1:].values.tolist() for i in sublist]
-        # # slice the unused columns
-        # release_row = release_row[4:]
-
-        # print(survey,release_row)
-
         for i in range(len(survey)):
             release = ''
-            # if release_row[ i] == '已釋出':
-            #     release = 1
-
             survey[i].append(release)
 
         return survey
@@ -228,8 +222,7 @@ class SurveyUpload(SQLManager):
             })
 
         problems.dropna(subset=['problem_name'], inplace=True)
-
-        # print(problems)
+        problems.fillna('',inplace=True)
 
         return problems
 
