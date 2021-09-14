d4b8e856a328f5e71e4264eb0534fd46cae660bc
Alanasdw <alanasdw@gmail.com>
2021-09-14 17:17:50 +0800

    Fix: changed the friends to relative

--- repo/utils.py
+++ repo/utils.py
@@ -1,4 +1,4 @@
 from bidict import bidict
 
-SURVEY_TYPE = bidict({'teacher': 1, 'parent': 2, 'friend': 3})
+SURVEY_TYPE = bidict({'teacher': 1, 'parent': 2, 'relative': 3})
 AGE_TYPE = bidict({'small': 1, 'big': 2})
