9d5647879e20c053cc78a2f6b9e9a76ef5d11777
Alanasdw <alanasdw@gmail.com>
2021-09-14 11:30:33 +0800

    Fix: added the convertion to lower of the used things in df and meta

--- repo/export/method.py
+++ repo/export/method.py
@@ -22,6 +22,8 @@ class MethodInterface(abc.ABC):
 class Union(MethodInterface):
     def concat_df(self, res, tmp, str_info):
         tmp['wave'] = str_info
+        # to lower
+        tmp.columns = [ x.lower() for x in tmp.columns]
         res = pandas.concat([res, tmp], ignore_index=True)
         return res
 
@@ -30,6 +32,12 @@ class Union(MethodInterface):
         tmp_var_labels = tmp.variable_value_labels
         tmp_col_names = tmp.column_names
         tmp_col_labels = tmp.column_labels
+
+        # to lower
+        tmp_org_type = { k.lower():v for k,v in tmp_org_type.items()}
+        tmp_var_labels = { k.lower():v for k,v in tmp_var_labels.items()}
+        tmp_col_names = [ x.lower() for x in tmp_col_names]
+        tmp_col_labels = [ x.lower() for x in tmp_col_labels]
 
         res['org_types'].update(tmp_org_type)
         for col, labels in tmp_var_labels.items():
@@ -54,6 +62,8 @@ class Join(MethodInterface):
 
     def concat_df(self, res, tmp, str_info):
         tmp.columns = [self.repl_except(c, str_info) for c in tmp.columns]
+        # to lower
+        tmp.columns = [ x.lower() for x in tmp.columns]
         if res.empty:
             res = tmp
         else:
@@ -68,6 +78,12 @@ class Join(MethodInterface):
         tmp_var_labels = tmp.variable_value_labels
         tmp_col_names = tmp.column_names
         tmp_col_labels = tmp.column_labels
+
+        # to lower
+        tmp_org_type = { k.lower():v for k,v in tmp_org_type.items()}
+        tmp_var_labels = { k.lower():v for k,v in tmp_var_labels.items()}
+        tmp_col_names = [ x.lower() for x in tmp_col_names]
+        tmp_col_labels = [ x.lower() for x in tmp_col_labels]
 
         for col, col_type in tmp_org_type.items():
             res['org_types'].update({self.repl_except(col, str_info) : col_type})
